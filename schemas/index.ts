26994c0b7c788e10cf59d6f3756743227dbc9ec7
Newbie465 <chiragbhatnagar465@gmail.com>
2023-06-29 18:51:26 +0530

    sanity configured and set

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import skills from './skills'
+import about from './about'
+import contact from './contact'
+import experiences from './experiences'
+import works from './works'
+import workExperience from './workExperience'
+
+export const schemaTypes = [skills, about, contact, workExperience, experiences, works]
